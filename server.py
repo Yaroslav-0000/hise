1e1c4f6b92e50f7faef5492cdd5558f30da7e386
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-01-27 10:58:52 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,12 @@
 import os
 from flask import Flask
-import requests
-import base64
 
 app = Flask(__name__)
 
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
-REPO = "Yaroslav-0000/hise"
-FILE_PATH = "server.py"   # указываем реальный файл
-
 @app.route("/")
 def home():
-    url = f"https://api.github.com/repos/{REPO}/contents/{FILE_PATH}"
-    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
-    r = requests.get(url, headers=headers)
-    data = r.json()
-    code = base64.b64decode(data["content"]).decode("utf-8")
-    return f"<pre>{code}</pre>"   # покажет код как текст в браузере
+    return "Сервер работает глобально ✅"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))  # Railway передаст свой порт
     app.run(host="0.0.0.0", port=port)
