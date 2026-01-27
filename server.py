346b753015f7dd8398ef8262a04d074601f3d9e5
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-01-27 10:15:06 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
 import os
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+GITHUBTOKEN = os.environ.get("GITHUBTOKEN")
+REPO = "Yaroslav-0000/Shop330"
+FILE_PATH = "index.html"
+
 @app.route("/")
 def home():
-    return "Сервер работает глобально ✅"
+    url = f"https://api.github.com/repos/{REPO}/contents/{FILE_PATH}"
+    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+    r = requests.get(url, headers=headers)
+    data = r.json()
+    import base64
+    html_code = base64.b64decode(data["content"]).decode("utf-8")
+    return html_code
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
